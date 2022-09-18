276166f1742cd8208661fa6607b1f45aeb0ee487
tirthangkards <111839376+tirthangkards@users.noreply.github.com>
2022-09-18 23:10:06 +0530

    Update Mazepath-D2.cpp

--- Mazepath-D2.cpp
+++ Mazepath-D2.cpp
@@ -1,66 +1,42 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void print(vector<vector<int>> temp) {
-    for(int i=0; i<temp.size(); i++) {
-        for(int j=0; j<temp[i].size(); j++) {
-            cout<<temp[i][j]<<" ";
-        }
-        cout<<endl;
-    }
-}
-
-void print_ans(vector<string> temp) {
-    for(string str: temp) {
-        cout<<str<<endl;
-    }
-}
-
-void solve(int x_axis, int y_axis, int n1, int n2, string move, vector<string> &ans, vector<vector<int>> visited) {
-
-    if(x_axis==n2 and y_axis==n1) {
-        ans.push_back(move);
+void lexicoLarger(string str, string osf, bool flag, string originalString)
+{
+    if (str.size() == 0)
+    {
+        if (osf > originalString)
+            cout << osf << endl;
         return;
     }
-    // Vertical Move
-    if(x_axis+1<n2 and visited[x_axis+1][y_axis]==0) {
-        solve(x_axis+1,y_axis,n1,n2,move+"V",ans,visited);
-        visited[x_axis][y_axis]==1;
+    for (int i = 0; i < str.size(); i++)
+    {
+        string ros = str.substr(0, i) + str.substr(i + 1);
+        char ch = str[i];
+        if (flag)
+        {
+            lexicoLarger(ros, osf + ch, flag, originalString);
+        }
+        else
+        {
+            if (str[i] >= str[0])
+            {
+                lexicoLarger(ros, osf + ch, flag or ch > str[0], originalString);
+            }
+            else if (str[i] < str[0])
+            {
+                // No call
+            }
+        }
     }
-    //  Horizontal Move
-    if(y_axis+1<n1 and visited[x_axis][y_axis+1]==0) {
-        solve(x_axis,y_axis+1,n1,n2,move+"H",ans,visited);
-        visited[x_axis][y_axis]==1;
-    }
-    // Diagonal Move
-    if(x_axis+1<n2 and y_axis<n1 and visited[x_axis+1][y_axis+1]==0) {
-        solve(x_axis+1,y_axis+1,n1,n2,move+"D",ans,visited);
-        visited[x_axis][y_axis]==1;
-    }    
 }
 
-int main() {
-
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    #endif
-
-    int n1;
-    cin>>n1;
-    int n2;
-    cin>>n2;
-
-    vector<int> row(n2,0);
-    vector<vector<int>> visited;
-    for(int i=0; i<n1; i++) {
-        visited.push_back(row);
-    }
-    vector<string> ans;
-    print(visited);
-    solve(0,0,n1,n2,"",ans,visited);
-    print_ans(ans);
-    cout<<ans.size()<<endl;
-
+int main(int argc, char const *argv[])
+{
+    string str;
+    cin >> str;
+    string orig = str;
+    sort(str.begin(), str.end());
+    lexicoLarger(str, "", false, orig);
     return 0;
 }
